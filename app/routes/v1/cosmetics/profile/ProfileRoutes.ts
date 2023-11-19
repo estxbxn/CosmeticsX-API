bc688b040c1b021182708857dc14a7a73647e88e
Estéban Piecourt <42072211+estxbxn@users.noreply.github.com>
2023-11-19 01:06:03 +0100

    (19/11) - Create ProfileRoutes

---
+++ app/routes/v1/cosmetics/profile/ProfileRoutes.ts
@@ -0,0 +1,29 @@
+import Route from "@ioc:Adonis/Core/Route";
+
+export class ProfileRoutes {
+  /*
+  |--------------------------------------------------------------------------
+  | Routes
+  | Allows to see a specific profile
+  |--------------------------------------------------------------------------
+  */
+  public static start(): void {
+    Route.get("api/v1/cosmetics/profile/:uuid", async ({ params }) => {
+      // const result = await DatabaseManager.select_all(
+      //   "elarys",
+      //   "beta-allowed",
+      //   {}
+      // );
+
+      const result = {
+        ok: false,
+        message:
+          "The profile requested with unique id " +
+          params.uuid +
+          " does not exist",
+      };
+      if (!result) return { error: "No ranks allowed matches" };
+      return result;
+    });
+  }
+}
